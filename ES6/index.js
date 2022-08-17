13d31efd00ead640f870b6662998c16e066212c4
veera950 <kalyankodari789@gmail.com>
2022-08-18 00:18:22 +0530

    JavaScript

--- ES6/index.js
+++ ES6/index.js
@@ -4,3 +4,19 @@ const age = 35;
 const sentence = 'My name is ${name}, and I am  ${age} years old';
 
 console.log(sentence);
+
+class Human {
+    constructor() {
+      this.gender = 'Male';
+    }
+    printGender() {
+      console.log(this.gender);
+    }
+  }
+  const human = new Human();
+  human.printGender();
+
+
+const numbers = [1, 2, 3];
+const newNumbers = [...numbers, 6];
+console.log(newNumbers);
